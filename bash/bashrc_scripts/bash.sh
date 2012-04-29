2be1cd8edd63efc6cc6bfda1110d4b854ee5812b
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-04-29 17:05:22 +1000

    Add left alignment of bash prompt
    Fixes issues with multiple line commands
    overflowing graphically.

--- bash/bashrc_scripts/bash.sh
+++ bash/bashrc_scripts/bash.sh
@@ -14,3 +14,6 @@ export HISTCONTROL=erasedups
 # expand the bash history to X commands
 export HISTSIZE=5000
 
+# Fix bash prompt mixing with multi line commands
+# this forces the prompt to be left aligned
+export PS1="\[\033[G\]$PS1"
