0778b7cc5f36a49f47377346950f60a718ef25b7
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-07-05 20:20:29 +1000

    Add possible fix for virtualenv error

--- bash/bashrc_scripts/python_virtualenv.sh
+++ bash/bashrc_scripts/python_virtualenv.sh
@@ -23,3 +23,14 @@ export PIP_VIRTUALENV_BASE=$WORKON_HOME
 # use eval to avoid the error "Could not find an activated virtualenv (required)."
 eval `PIP_REQUIRE_VIRTUALENV= pip completion --bash`
 
+# possible fix for following error
+#Traceback (most recent call last):
+#  File "/Users/[username]/.pythonbrew/pythons/Python-2.7.2/lib/python2.7/logging/handlers.py", line 78, in emit
+#      self.doRollover()
+#	    File "/Users/[username]/.pythonbrew/pythons/Python-2.7.2/lib/python2.7/logging/handlers.py", line 141, in
+#		doRollover
+#		    os.rename(self.baseFilename, dfn)
+#			OSError: [Errno 2] No such file or directory
+#			Logged from file hook_loader.py, line 117
+export VIRTUALENVWRAPPER_LOG_DIR=/tmp
+
