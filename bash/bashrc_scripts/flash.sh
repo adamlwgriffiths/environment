9ec8ac415a5b94b6e2cd0bf3217d09e855997ff7
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-01-04 14:15:00 +1100

    Add flash SDK script.

---
+++ bash/bashrc_scripts/flash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#-------------------------------------------------------------
+# SDK paths
+#-------------------------------------------------------------
+
+# Add the Flex/AIR SDK to our path
+export PATH=$PATH:~/Workspace/libs/flex_sdk_4.6_air_3.5/bin
+
