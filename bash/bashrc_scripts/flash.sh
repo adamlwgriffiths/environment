4285dfcbb8c9f74173e712b16d9b840b277db1c7
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-03-21 16:41:52 +1100

    Update AIR SDK, add to PATH

--- bash/bashrc_scripts/flash.sh
+++ bash/bashrc_scripts/flash.sh
@@ -5,5 +5,7 @@
 #-------------------------------------------------------------
 
 # Add the Flex/AIR SDK to our path
-export PATH=$PATH:~/Workspace/libs/flex_sdk_4.6_air_3.5/bin
+#export AIR_SDK=~/Workspace/libs/flex_sdk_4.6.0.23201B-AIR_3.4
+export AIR_SDK=~/Workspace/libs/flex_sdk_4.6_air_3.6
+export PATH=$PATH:$AIR_SDK/bin
 
