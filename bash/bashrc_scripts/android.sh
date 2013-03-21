913dde33c71ba55276281cb31ec4c09447c18f3c
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-03-21 16:42:02 +1100

    Add android env script.

---
+++ bash/bashrc_scripts/android.sh
@@ -0,0 +1,9 @@
+# define NDK for https://github.com/halfninja/android-ffmpeg-x264
+export NDK=~/workspace/libs/android-ndk-r8d
+
+# define NDK_BASE for https://github.com/guardianproject/android-ffmpeg
+export NDK_BASE=$NDK
+
+# add to PATH
+export PATH=$PATH:$NDK
+
