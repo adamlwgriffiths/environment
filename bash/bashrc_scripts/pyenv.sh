7b24c342855450c66bc4c606ae6c539d8228600c
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-11-26 22:21:02 +1100

    chmod -x android / flash, add pyenv, update venv.

--- bash/bashrc_scripts/pyenv.sh
+++ bash/bashrc_scripts/pyenv.sh
@@ -4,14 +4,23 @@
 # Python definitions
 #-------------------------------------------------------------
 
+eval "$(pyenv init -)"
+
+# Virtualenvwrapper
+# virtualenv wrapper support
+export WORKON_HOME=~/Workspace/VirtualEnvs
+export PROJECT_HOME=~/Workspace
+
 # PIP
 # tell pip to only install inside virtualenvs
-export PIP_REQUIRE_VIRTUALENV=true
+#export PIP_REQUIRE_VIRTUALENV=true
 
 # make pip use the virtualenv dir
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 
 # add pip bash completion
 # use eval to avoid the error "Could not find an activated virtualenv (required)."
-eval `pip completion --bash`
+#eval `PIP_REQUIRE_VIRTUALENV= pip completion --bash`
+
+pyenv virtualenvwrapper
 
